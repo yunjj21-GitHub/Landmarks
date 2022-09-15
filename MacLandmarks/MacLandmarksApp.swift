fd8a19f2e2ba8584db56585b15d2cf2cd701c64c
yunjj21 <yunjj21@hs.ac.kr>
2022-09-15 14:02:55 +0900

    Creating a macOS App

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by RP Kyobo on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
